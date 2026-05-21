330f0a858bdb9098bc434846115edde1067c39ac
mdraconsultancy <mdraconsultancy@gmail.com>
2026-05-22 00:46:04 +0530

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,10 @@
-import type { Config } from 'drizzle-kit';
+import { defineConfig } from 'drizzle-kit';
 
-export default {
+export default defineConfig({
   schema: './lib/schema.ts',
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL_UNPOOLED!,
   },
-} satisfies Config;
+});
