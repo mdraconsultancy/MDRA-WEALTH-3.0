b252490ae7d35dacca696e73f6d71293bf026355
mdraconsultancy <mdraconsultancy@gmail.com>
2026-05-22 13:07:08 +0530

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'drizzle-kit';
+import * as dotenv from 'dotenv';
+
+// Force Drizzle to read your Next.js environment file
+dotenv.config({ path: '.env.local' });
 
 export default defineConfig({
   schema: './lib/schema.ts',
   out: './drizzle',
-  dialect: 'postgresql',
+  dialect: 'postgresql', // Make sure this says postgresql
   dbCredentials: {
-    url: process.env.DATABASE_URL_UNPOOLED!,
+    url: process.env.DATABASE_URL!,
   },
 });
